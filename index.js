40c0c2a2a2a34785078015b4c0433998389a5e19
Koji Osugi <koji097@gmail.com>
2018-12-31 23:33:28 -0200

    Funcao anonima e Arrow Function

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function calc(x1, x2, operator) {
+
+//Funcao anonima
+(function(x1, x2, operator) {
 
     return eval(`${x1} ${operator} ${x2}`); //Funcao eval: Pega uma String e o interpreta
 
+})(1, 2, '*');
+
+//Arrow Function
+let calc = (x1, x2, operator) => {
+    return eval(`${x1} ${operator} ${x2}`);
 }
 
 let resultado = calc(1, 2, '+');
